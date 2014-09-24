871399a9644c54a0eb144ecc6e5813ca7317c3ea
linksapprentice1 <linksapprentice1@gmail.com>
2014-09-23 23:17:55 -0700

    Data is now charted!

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 from __future__ import division
 import NewsSentiment
 import threader
+import pygal
+import webbrowser
 
-def printSentiment(news, term):
-   print news.__class__.__name__ + ": " + str(average(news.getSentiments(term)))
+def makeChart(term):
+   chart = pygal.Box(range=(-1, 1))
+   chart.title = 'News Sentiments for Term: ' + term
 
-def average(nums):
-   return sum(nums)/len(nums)
+   cnn = NewsSentiment.Cnn()
+   fox = NewsSentiment.Fox()
+
+   threads = [threader.makeThread(generateSentiment, (chart, cnn, term)),
+              threader.makeThread(generateSentiment, (chart, fox, term))]
+   threader.joinThreads(threads)
+
+   filename = term + '.png'
+   chart.render_to_png(filename)
+   webbrowser.open(filename)
+
+def generateSentiment(chart, news, term):
+   sentiments = [round(sentiment, 2) for sentiment in news.getSentiments(term)]
+   chart.add(news.__class__.__name__ , sentiments)
 
 term = raw_input("Enter a term: ")
-
-cnn = NewsSentiment.Cnn()
-fox = NewsSentiment.Fox()
-
-threads = [threader.makeThread(printSentiment, (cnn, term)),
-          threader.makeThread(printSentiment, (fox, term))]
-
-threader.joinThreads(threads)
+makeChart(term)
